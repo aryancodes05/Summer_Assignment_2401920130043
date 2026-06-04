5d806109de3528e95130556f90f02fa4e6bb2aec
Aryan Prajapati <104238706+aryancodes05@users.noreply.github.com>
2026-06-05 01:09:03 +0530

    Update Code_1.cpp

--- Week_1/Day_4/Code_1.cpp
+++ Week_1/Day_4/Code_1.cpp
@@ -1,1 +1,17 @@
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& matrix) {
+        int sum = 0;
+        int n = matrix.size();
 
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i == j || i + j == n - 1) {
+                    sum= sum+ matrix[i][j];
+                }
+            }
+        }
+
+        return sum;
+    }
+};
