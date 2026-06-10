9ca8e9e9c7e3c35ddf6330fc77479b4389bfc974
Aryan Prajapati <104238706+aryancodes05@users.noreply.github.com>
2026-06-10 22:54:51 +0530

    Update code2.cpp

--- Week_2/Day_2/code2.cpp
+++ Week_2/Day_2/code2.cpp
@@ -1,1 +1,25 @@
 
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        
+        unordered_set<char> st;
+        
+        int i = 0, j = 0, ans = 0;
+
+        while(j < s.length()) {
+
+            if(st.find(s[j]) == st.end()) {
+                st.insert(s[j]);
+                ans = max(ans, j - i + 1);
+                j++;
+            }
+            else {
+                st.erase(s[i]);
+                i++;
+            }
+        }
+
+        return ans;
+    }
+};
