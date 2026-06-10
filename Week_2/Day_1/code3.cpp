85084b580675c4dd9d644b82c3f66770535210da
Aryan Prajapati <104238706+aryancodes05@users.noreply.github.com>
2026-06-10 22:52:09 +0530

    Update code3.cpp

--- Week_2/Day_1/code3.cpp
+++ Week_2/Day_1/code3.cpp
@@ -1,1 +1,22 @@
+class Solution {
+public:
+    bool isAnagram(string s1, string s2) {
+        
+        if(s1.length() != s2.length()) {
+            return false;
+        }
 
+        sort(s1.begin(), s1.end());
+        sort(s2.begin(), s2.end());
+
+        int n = s1.length();
+
+        for(int i = 0; i < n; i++) {
+            if(s1[i] != s2[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
